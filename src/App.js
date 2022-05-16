8806bea563a812a95d406f98d766c4a652add2a5
Tom Hickz <999194+tomhicz@users.noreply.github.com>
2022-05-16 12:28:46 +0900

    Handle rounding

--- src/App.js
+++ src/App.js
@@ -4,17 +4,26 @@ import "./App.css";
 
 function App() {
   //State
+  //Always change these using handleChange functions to keep in sync
   const [tempF, setTempF] = useState(75);
   const [tempC, setTempC] = useState(25);
+
+  const maxAccuracy = 2;
+
+  function roundMaxPlaces(num, places) {
+    //Round to the max number of decimal places, but only if needed
+    const p = Math.pow(10, places);
+    return Math.round(num * p) / p;
+  }
 
   //Handlers
   function handleChangeF(e) {
     setTempF(e.target.value);
-    setTempC((e.target.value - 32) / 1.8);
+    setTempC(roundMaxPlaces((e.target.value - 32) / 1.8, maxAccuracy));
   }
   function handleChangeC(e) {
     setTempC(e.target.value);
-    setTempF(e.target.value * 1.8 + 32);
+    setTempF(roundMaxPlaces(e.target.value * 1.8 + 32, maxAccuracy));
   }
 
   return (
